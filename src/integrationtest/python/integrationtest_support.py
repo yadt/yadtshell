77a6131313969649ad722ce51cfe9ad4765af114
Michael Gruber <aelgru@gmail.com>
2012-08-28 11:13:39 +0200

    which python

--- src/integrationtest/python/integrationtest_support.py
+++ src/integrationtest/python/integrationtest_support.py
@@ -19,14 +19,11 @@ __author__ = 'Alexander Metzner, Michael Gruber, Udo Juettner'
 import sys
 import inspect
 
-from os import environ
-from os import symlink
-from os import pathsep as path_separator
-from os import sep as file_separator
+from os import environ, pathsep, symlink, sep
 from os.path import abspath, exists, join
 
 from shutil import rmtree
- 
+
 from shtub import testbase
 
 
@@ -41,7 +38,7 @@ class IntegrationTestSupport (testbase.IntegrationTestBase):
         self.prepare_testbed(self._create_env(), ['ssh'])
         self._make_yadtshell_testable()
 
-        
+
     def write_target_file (self, *hostnames):
         target_filename = join(self.base_dir, 'target')
         with open(target_filename, 'w') as target_file:
@@ -55,43 +52,43 @@ class IntegrationTestSupport (testbase.IntegrationTestBase):
     def _make_yadtshell_testable (self):
         absolute_path_to_script = abspath(join('src', 'main', 'scripts', 'yadtshell'))
         destination = join(self.stubs_dir, 'yadtshell')
-        
+
         symlink(absolute_path_to_script, destination)
 
 
     def _create_target_directory(self, name):
-        target_dir = abspath(__file__).split(file_separator)[:-4]
+        target_dir = abspath(__file__).split(sep)[:-4]
         target_dir.append('target')
         target_dir.append('integrationtests')
         target_dir.append(name)
-        
-        base_dir = file_separator.join(target_dir)
-        
+
+        base_dir = sep.join(target_dir)
+
         if exists(base_dir):
             rmtree(base_dir)
-            
+
         self.make_base_dir(base_dir)
 
 
     def _create_path(self):
-        path = self.stubs_dir 
-        if environ.has_key('PATH'):
-            path += path_separator + environ['PATH']
+        path = self.stubs_dir
+        if 'PATH' in environ:
+            path += pathsep + environ['PATH']
         else:
-            path += path_separator + '/bin'
-            path += path_separator + '/usr/bin'
-            path += path_separator + '/usr/local/bin'
+            path += pathsep + '/bin'
+            path += pathsep + '/usr/bin'
+            path += pathsep + '/usr/local/bin'
         return path
 
 
     def _create_python_path(self):
-        python_path = path_separator.join(sys.path)
+        python_path = pathsep.join(sys.path)
 
         if environ.has_key('PYTHONPATH'):
-            python_path += path_separator + environ['PYTHONPATH']
+            python_path += pathsep + environ['PYTHONPATH']
 
-        python_path += path_separator + abspath(join('..', 'hostexpand', 'src', 'main', 'python'))
-        python_path += path_separator + abspath(join('src', 'main', 'python'))
+        python_path += pathsep + abspath(join('..', 'hostexpand', 'src', 'main', 'python'))
+        python_path += pathsep + abspath(join('src', 'main', 'python'))
         return python_path
 
 
