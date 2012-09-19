0ffbab80670c381c9695931f33a28e77ced68d6f
halimath <halimath.wilanthaou@googlemail.com>
2012-09-19 14:23:00 +0200

    using pybuilder environments for building with teamcity

--- build.py
+++ build.py
@@ -14,40 +14,33 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
-
 from pythonbuilder.core import use_plugin, init, Author
 
 use_plugin('python.core')
-#use_plugin('python.unittest')
 use_plugin('python.integrationtest')
 use_plugin('python.install_dependencies')
 
-#use_plugin('python.coverage')
-#use_plugin('python.pychecker')
-#use_plugin('python.pylint')
 use_plugin('python.distutils')
 use_plugin('python.pydev')
-use_plugin('analysis')
 
 use_plugin('copy_resources')
 use_plugin('filter_resources')
 
 
 authors     = [Author('Arne Hilmann', 'arne.hilmann@gmail.com')]
-description = """YADT - an Augmented Deployment Tool - The Shell Part
+description = '''YADT - an Augmented Deployment Tool - The Shell Part
 - regards the dependencies between services, over different hosts
 - updates artefacts in a safe manner
 - issues multiple commands in parallel on severall hosts
 
 for more documentation, visit http://code.google.com/p/yadt/wiki/YadtCommands
-"""
+'''
 
 name    = 'yadtshell'
 license = 'GNU GPL v3'
 summary = 'YADT - an Augmented Deployment Tool - The Shell Part'
 url     = 'https://github.com/yadt/yadtshell'
-version = '1.3.12-%s' % os.environ.get('BUILD_NUMBER', 0)
+version = '1.3.12'
  
 default_task = ['publish']
 
@@ -57,11 +50,8 @@ def set_properties (project):
     project.depends_on('Twisted')
     project.depends_on('PyYAML')
 
-    project.build_depends_on('coverage')
     project.build_depends_on('shtub', url='https://github.com/downloads/yadt/shtub/shtub-0.2.9.tar.gz')
 
-    project.set_property('coverage_break_build', False)
-    project.set_property('pychecker_break_build', False)
     project.set_property('integration_test_print_err', True)
 
     project.set_property('copy_resources_target', '$dir_dist')
@@ -81,3 +71,10 @@ def set_properties (project):
         'Topic :: System :: Software Distribution',
         'Topic :: System :: Systems Administration'
     ])
+    
+@init(environments='teamcity')
+def set_properties_for_teamcity_builds (project):
+    import os
+    project.version = '%s-%s' % (project.version, os.environ.get('BUILD_NUMBER', 0))
+    project.default_task = ['install_build_dependencies', 'publish']
+
