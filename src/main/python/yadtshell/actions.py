d263bef580b84c32b9d0bf536d360ac2339dd780
Michael Gruber <aelgru@gmail.com>
2013-01-29 11:11:35 +0100

    GNU General Public License header

--- src/main/python/yadtshell/actions.py
+++ src/main/python/yadtshell/actions.py
@@ -1,4 +1,21 @@
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+#
+#   YADT - an Augmented Deployment Tool
+#   Copyright (C) 2010-2013  Immobilien Scout GmbH
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 class ActionException(Exception):
     def __init__(self, message, exitcode, rootcause=None):
         self.message = message
