c0e57c63638ef3a84537f01d27d6e0258b333621
Michael Gruber <aelgru@gmail.com>
2013-02-01 16:15:49 +0100

    enabling coverage plugin

--- build.py
+++ build.py
@@ -20,6 +20,7 @@ use_plugin('python.core')
 use_plugin('python.integrationtest')
 use_plugin('python.install_dependencies')
 use_plugin('python.unittest')
+use_plugin('python.coverage')
 
 use_plugin('python.distutils')
 use_plugin('python.pydev')
