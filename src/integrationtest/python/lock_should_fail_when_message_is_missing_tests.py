056c357259e84adf51a3dbb854fb9c8ba96e73a6
Michael Gruber <aelgru@gmail.com>
2013-02-11 13:35:02 +0100

    added integration test: lock_should_fail_when_message_is_missing_tests

---
+++ src/integrationtest/python/lock_should_fail_when_message_is_missing_tests.py
@@ -0,0 +1,31 @@
+#   YADT - an Augmented Deployment Tool
+#   Copyright (C) 2010-2013  Immobilien Scout GmbH
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = 'Michael Gruber, Maximilien Riehl'
+
+import unittest
+import integrationtest_support
+
+
+class Test (integrationtest_support.IntegrationTestSupport):
+    def test(self):
+        actual_return_code = self.execute_command('yadtshell lock host://spameggs')
+
+        self.assertEquals(12, actual_return_code)
+
+
+if __name__ == '__main__':
+    unittest.main()
