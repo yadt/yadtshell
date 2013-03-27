2553822fc3ca6cbf47b163996036f3dc01426959
mriehl <maximilien.riehl@gmail.com>
2013-03-27 14:55:33 +0100

    trying to add helpful error messages when component uri cannot be resolved

---
+++ src/integrationtest/python/updateartefact_should_fail_when_uri_cannot_be_resolved.py
@@ -0,0 +1,47 @@
+#   YADT - an Augmented Deployment Tool
+#   Copyright (C) 2010-2013  Immobilien Scout GmbH
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = 'Maximilien Riehl'
+
+import unittest
+import integrationtest_support
+
+import yadt_status_answer
+
+
+class Test (integrationtest_support.IntegrationTestSupport):
+    def test (self):
+        self.write_target_file('it01.domain')
+
+        with self.fixture() as when:
+            when.calling('ssh').at_least_with_arguments('it01.domain').and_input('/usr/bin/yadt-status') \
+                .then_write(yadt_status_answer.stdout('it01.domain'))
+            when.calling('ssh').at_least_with_arguments('it01.domain') \
+                .then_return(0)
+
+        status_return_code = self.execute_command('yadtshell status -v')
+        update_return_code = self.execute_command('yadtshell updateartefact artefact://*/*/// -v')  # invalid due to
+                                                                                                    # trailing slashes
+
+        with self.verify() as verify:
+            self.assertEquals(0, status_return_code)
+            verify.called('ssh').at_least_with_arguments('it01.domain').and_input('/usr/bin/yadt-status')
+
+            self.assertEquals(1, update_return_code)
+
+
+if __name__ == '__main__':
+    unittest.main()
