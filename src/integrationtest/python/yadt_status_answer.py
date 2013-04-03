9698efb6760129ce7c64e467a79b4ac20670bd80
mriehl <maximilien.riehl@gmail.com>
2013-04-03 09:44:36 +0200

    adding support for different templates since templating service dependencies == hell

--- src/integrationtest/python/yadt_status_answer.py
+++ src/integrationtest/python/yadt_status_answer.py
@@ -68,8 +68,59 @@ state: uptodate
 query_time: 1
 """)
 
+STATUS_TEMPLATE_WITH_ARTIFACT_SERVICE_DEPENDENCIES = string.Template("""
+hostname: "$host"
+fqdn: "$host_fqdn"
+uptime: " 10:02:31 up 156 days, 4:57, 0 users, load average: 0.38, 0.12, 0.09"
+date: "$date"
+epoch: $timestamp
+ip: "127.0.0.1"
+interface:
+  lo0: 127.0.0.1
+pwd: /home/yadt-integration-test-user
+age_of_cached_structure: 1
+defaults:
+  YADT_EXITCODE_HOST_LOCKED: 150
+  YADT_EXITCODE_SERVICE_IGNORED: 151
+  YADT_LOCK_DIR: /var/lock/yadt
+
+services:
+- frontend-service:
+    needs_services: [backend-service]
+    state: $frontend_service_state
+    service_artefact: yit-frontend-service
+    toplevel_artefacts: [yit-config-$host]
+- backend-service:
+    state: $backend_service_state
+    service_artefact: yit-backend-service
+    toplevel_artefacts: [yit-config-$host]
+    needs_artefacts: [yit-config-$host]
+
+artefact_names_handled_by_yadt:
+- yit-frontend-service
+- yit-backend-service
+- yit-config-$host
+current_artefacts:
+- yit-frontend-service/0:0.0.1-1
+- yit-backend-service/0:0.0.1-1
+- yit-config-$host/0:0.0.1-1
+handled_artefacts:
+- yit-frontend-service/0:0.0.1-1
+- yit-backend-service/0:0.0.1-1
+- yit-config-$host/0:0.0.1-1
+age_of_cached_artefacts: 1
+next_artefacts:
+- yit-config-$host/0:0.0.1-2
+artefacts_query_epoch: $timestamp
+state: update_needed
+query_time: 1
+""")
 
 def stdout (host, frontend_service_state=0, backend_service_state=0):
+    return stdout_with_template(host, frontend_service_state, backend_service_state, template=STATUS_TEMPLATE)
+
+
+def stdout_with_template(host, frontend_service_state=0, backend_service_state=0, template=STATUS_TEMPLATE):
     date = datetime.datetime.now().strftime("%a %b %d %H:%M:%S %Z %Y")
 
     placeholders = {
@@ -80,4 +131,4 @@ def stdout (host, frontend_service_state=0, backend_service_state=0):
         'host_fqdn'              : host,
         'timestamp'              : int(time.time())}
 
-    return STATUS_TEMPLATE.substitute(placeholders)
+    return template.substitute(placeholders)
