db727786c34692f7e626c0f6b1b740fffcdc6477
Marcel Wolf <marcel.wolf@immobilienscout24.de>
2013-04-15 13:04:23 +0200

    adding test for parallel stop of services

---
+++ src/integrationtest/python/parallel_stop_should_stop_services_in_paralellel_tests.py
@@ -0,0 +1,64 @@
+#   YADT - an Augmented Deployment Tool
+#   Copyright (C) 2010-2013  Immobilien Scout GmbH
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = 'Arne Hilmann, Marcel Wolf'
+
+import unittest
+import integrationtest_support
+
+import yadt_status_answer
+
+
+class Test (integrationtest_support.IntegrationTestSupport):
+    def test (self):
+        self.write_target_file(['it01.domain', 'it02.domain'])
+
+        with self.fixture() as when:
+            when.calling('ssh').at_least_with_arguments('it01.domain').and_input( '/usr/bin/yadt-status') \
+                .then_write(yadt_status_answer.stdout('it01.domain'))
+            when.calling('ssh').at_least_with_arguments('it02.domain').and_input( '/usr/bin/yadt-status') \
+                .then_write(yadt_status_answer.stdout('it02.domain'))
+            when.calling('ssh').at_least_with_arguments('it01.domain', 'sudo /sbin/service frontend-service status').and_input('status') \
+                .then_return(1, milliseconds_to_wait=4000)
+            when.calling('ssh').at_least_with_arguments('it02.domain', 'sudo /sbin/service frontend-service status').and_input('status') \
+                .then_return(1, milliseconds_to_wait=4000)
+            when.calling('ssh').at_least_with_arguments('it01.domain') \
+                .then_return(0)
+            when.calling('ssh').at_least_with_arguments('it02.domain') \
+                .then_return(0)
+
+        status_return_code = self.execute_command('yadtshell status -v')
+        stop1_return_code   = self.execute_command('yadtshell stop service://it01/frontend-service -v')
+        stop2_return_code   = self.execute_command('yadtshell stop service://it02/frontend-service -v')
+
+        with self.verify() as verify:
+            self.assertEquals(0, status_return_code)
+            verify.called('ssh').at_least_with_arguments('it01.domain').and_input('/usr/bin/yadt-status')
+            verify.called('ssh').at_least_with_arguments('it02.domain').and_input('/usr/bin/yadt-status')
+
+            self.assertEquals(0, stop1_return_code)
+            verify.called('ssh').at_least_with_arguments('it01.domain', '-O', 'check')
+            verify.called('ssh').at_least_with_arguments('it01.domain', 'sudo /sbin/service frontend-service stop').and_input('stop')
+            verify.called('ssh').at_least_with_arguments('it01.domain', 'sudo /sbin/service frontend-service status').and_input('status')
+            verify.called('ssh').at_least_with_arguments('it01.domain', '-O', 'exit')
+            self.assertEquals(0, stop2_return_code)
+            verify.called('ssh').at_least_with_arguments('it02.domain', '-O', 'check')
+            verify.called('ssh').at_least_with_arguments('it02.domain', 'sudo /sbin/service frontend-service stop').and_input('stop')
+            verify.called('ssh').at_least_with_arguments('it02.domain', 'sudo /sbin/service frontend-service status').and_input('status')
+            verify.called('ssh').at_least_with_arguments('it02.domain', '-O', 'exit')
+
+if __name__ == '__main__':
+    unittest.main()
