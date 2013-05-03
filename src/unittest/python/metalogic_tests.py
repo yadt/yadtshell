7a332923f83703ef4cb543d731d765b229559b11
mriehl <maximilien.riehl@gmail.com>
2013-05-03 11:04:26 +0200

    testing plan augmentation with workers and errors tolerated

--- src/unittest/python/metalogic_tests.py
+++ src/unittest/python/metalogic_tests.py
@@ -26,4 +26,16 @@ class MetalogicTests(unittest.TestCase):
 
         self.assertEqual(actual_plan.nr_workers, 1)
 
+    def test_apply_instructions_should_augment_plan_with_workers_and_tolerated_errors(self):
+        actions = [Action('sudo service bar stop', 'service://foo/bar'),
+                   Action('sudo service baz stop', 'service://foo/baz')]
+        original_plan = ActionPlan('update', actions)
+
+        self.assertEqual(original_plan.nr_workers, None)
+
+        actual_plan = apply_instructions(original_plan, 'update=1_1_3')
+
+        self.assertEqual(actual_plan.actions[0].nr_workers, 1)
+        self.assertEqual(actual_plan.actions[0].nr_errors_tolerated, '3')
+
 
