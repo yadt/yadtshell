04e718c574b10a8a5993d6d3178b2087ff6fb5bc
mriehl <maximilien.riehl@gmail.com>
2013-05-03 10:46:52 +0200

    first test for augmenting plan with instructions

---
+++ src/unittest/python/metalogic_tests.py
@@ -0,0 +1,18 @@
+import unittest
+from yadtshell.metalogic import apply_instructions
+from yadtshell.actions import ActionPlan, Action
+
+class MetalogicTests(unittest.TestCase):
+
+    def test_apply_instructions_should_augment_plan_with_multiple_workers(self):
+        actions = [Action('sudo service bar stop', 'service://foo/bar'), Action('sudo service baz stop', 'service://foo/baz')]
+        original_plan = ActionPlan('update', actions)
+
+        self.assertEqual(original_plan.nr_workers, None)
+
+        actual_plan = apply_instructions(original_plan, 99)
+
+        self.assertEqual(actual_plan.nr_workers, 99)
+
+
+
