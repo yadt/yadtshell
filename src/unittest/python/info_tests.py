dafa4d5625ff7b035cd5373f94b957daf4e9d1ad
Arne Hilmann <arne@hilmann.de>
2013-06-06 14:57:43 +0200

    displaying updates correctly, based on new yadt-minion output

---
+++ src/unittest/python/info_tests.py
@@ -0,0 +1,38 @@
+import unittest
+
+import yadtshell
+from yadtshell.info import (highlight_differences)
+
+
+class ValidateHighlightingTest(unittest.TestCase):
+
+    def test_should_highlight_nothing_when_no_difference(self):
+        text = highlight_differences("foo", "foo")
+        self.assertEqual("foo", text)
+
+    def test_should_highlight_last_character_when_they_differ(self):
+        text = highlight_differences("foo", "foX")
+        self.assertEqual("fo${REVERSE}X${NORMAL}", text)
+
+    def test_should_highlight_everything_when_strings_mismatch_totally(self):
+        text = highlight_differences("foo", "bar")
+        self.assertEqual("${REVERSE}bar${NORMAL}", text)
+
+    def test_should_highlight_everything_when_strings_mismatch_in_first_position_only(self):
+        text = highlight_differences("foo", "boo")
+        self.assertEqual("${REVERSE}boo${NORMAL}", text)
+
+    def test_should_highlight_every_but_the_first_character_when_strings_differ_in_position_two(self):
+        text = highlight_differences("foo", "fXo")
+        self.assertEqual("f${REVERSE}Xo${NORMAL}", text)
+
+    def test_should_highlight_when_string_lengths_differ(self):
+        text = highlight_differences("foo", "fo1234567890")
+        self.assertEqual("fo${REVERSE}1234567890${NORMAL}", text)
+
+    def test_should_highlight_when_string_lengths_differ_again(self):
+        text = highlight_differences("fo1234567890", "foo")
+        self.assertEqual("fo${REVERSE}o${NORMAL}", text)
+
+if __name__ == '__main__':
+    unittest.main()
