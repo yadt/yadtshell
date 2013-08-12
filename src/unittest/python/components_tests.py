5fc68cebd029d0f8897ca44f07a0c82e163139dc
mriehl <maximilien.riehl@gmail.com>
2013-08-12 16:18:27 +0200

    increasing test coverage for components

---
+++ src/unittest/python/components_tests.py
@@ -0,0 +1,113 @@
+import unittest
+from mock import Mock, patch
+import yadtshell
+
+
+class ServiceTests(unittest.TestCase):
+
+    def setUp(self):
+        mock_service = Mock(yadtshell.components.Service)
+        mock_service._retrieve_service_call = lambda action: action
+        mock_service.name = 'internet'
+        self.mock_service = mock_service
+
+    def test_should_stop_service(self):
+        yadtshell.components.Service.stop(self.mock_service)
+
+        self.mock_service.remote_call.assert_called_with(
+            'stop', 'internet_stop', False)
+
+    def test_should_probe_service(self):
+        yadtshell.components.Service.status(self.mock_service)
+
+        self.mock_service.remote_call.assert_called_with(
+            'status', tag='internet_status')
+
+    def test_should_start_service(self):
+        yadtshell.components.Service.start(self.mock_service)
+
+        self.mock_service.remote_call.assert_called_with(
+            'start', 'internet_start', False)
+
+    def test_should_ignore_service(self):
+        yadtshell.components.Service.ignore(
+            self.mock_service, 'yes we can ignore the service')
+
+        self.mock_service.remote_call.assert_called_with(
+            'yadt-service-ignore internet "yes we can ignore the service"', 'ignore_internet', False)
+
+    def test_should_unignore_service(self):
+        yadtshell.components.Service.unignore(self.mock_service)
+
+        self.mock_service.remote_call.assert_called_with(
+            'yadt-service-unignore internet', 'unignore_internet')
+
+    def test_retrieve_service_call_should_build_command_with_action_and_name(self):
+        service_call = yadtshell.components.Service._retrieve_service_call(
+            self.mock_service, 'test')
+
+        self.assertEqual(service_call, 'yadt-service-test internet')
+
+
+class ArtefactTests(unittest.TestCase):
+
+    def test_should_update_artefacts(self):
+        mock_artefact = Mock(yadtshell.components.Artefact)
+        mock_artefact.name = 'ham'
+        mock_artefact.host = 'foobar42.domain'
+
+        yadtshell.components.Artefact.updateartefact(mock_artefact)
+
+        mock_artefact.remote_call.assert_called_with(
+            'yadt-artefact-update ham', 'artefact_foobar42.domain_ham_updateartefact')
+
+
+class HostTests(unittest.TestCase):
+
+    def test_should_update_next_artefacts_only(self):
+        mock_host = Mock(yadtshell.components.Host)
+        mock_host.next_artefacts = ['foo/1-2.3', 'bar/1-1.3/2']
+        mock_host.hostname = 'foobar42.domain'
+
+        yadtshell.components.Host.update(mock_host)
+
+        mock_host.remote_call.assert_called_with(
+            'yadt-host-update foo-1-2.3 bar-1-1.3/2', 'foobar42.domain_update')
+
+    def test_should_lock_host(self):
+        mock_host = Mock(yadtshell.components.Host)
+        mock_host.hostname = 'foobar42.domain'
+
+        yadtshell.components.Host.lock(
+            mock_host, message='lock me!', force=True)
+
+        mock_host.remote_call.assert_called_with(
+            "yadt-host-lock 'lock me!'", 'lock_host', True)
+
+    @patch('yadtshell.util.get_locking_user_info')
+    def test_remote_call_should_create_wrapping_command_with_adequate_environment(self, mock_lockinfo):
+        mock_lockinfo.return_value = {'owner': 'badass'}
+        yadtshell.settings.SSH = 'super-ssh'
+        mock_host = Mock(yadtshell.components.Host)
+        mock_host.create_remote_log_filename.return_value = 'logfilename'
+        mock_host.fqdn = 'foobar42.domain'
+        mock_host.name = 'foobar42'
+
+        command = yadtshell.components.Host.remote_call(mock_host, 'test')
+
+        self.assertEqual(
+            command, 'super-ssh foobar42.domain WHO="badass" YADT_LOG_FILE="logfilename" "yadt-command test" ')
+
+    @patch('yadtshell.util.get_locking_user_info')
+    def test_remote_call_should_use_host_when_component_has_no_fqdn(self, mock_lockinfo):
+        mock_lockinfo.return_value = {'owner': 'badass'}
+        yadtshell.settings.SSH = 'super-ssh'
+        mock_host = Mock(yadtshell.components.Host)
+        mock_host.create_remote_log_filename.return_value = 'logfilename'
+        mock_host.host = 'foobar42'
+        mock_host.name = 'foobar42'
+
+        command = yadtshell.components.Host.remote_call(mock_host, 'test')
+
+        self.assertEqual(
+            command, 'super-ssh foobar42 WHO="badass" YADT_LOG_FILE="logfilename" "yadt-command test" ')
