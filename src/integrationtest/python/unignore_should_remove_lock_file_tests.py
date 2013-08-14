57d0ed12d51809976878ee77b7caf4d8c15efec6
mriehl <maximilien.riehl@gmail.com>
2013-08-14 11:15:21 +0200

    adding possibility to skip the final status after a command, also with integration test

--- src/integrationtest/python/unignore_should_remove_lock_file_tests.py
+++ src/integrationtest/python/unignore_should_remove_lock_file_tests.py
@@ -50,6 +50,8 @@ class Test (integrationtest_support.IntegrationTestSupport):
             verify.called('ssh').at_least_with_arguments(
                 'it01.domain', 'yadt-command yadt-service-unignore frontend-service')
 
+            verify.called('ssh').at_least_with_arguments(
+                'it01.domain').and_input('/usr/bin/yadt-status')
 
 if __name__ == '__main__':
     unittest.main()
