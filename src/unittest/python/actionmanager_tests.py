c96b981d8f8d2df59703c42a815bd25bcc3c4c3c
mriehl <maximilien.riehl@gmail.com>
2013-08-19 16:57:26 +0200

    ensuring that there are no prompts in absence of a tty or in the case of a dryrun

--- src/unittest/python/actionmanager_tests.py
+++ src/unittest/python/actionmanager_tests.py
@@ -3,7 +3,32 @@ from mock import Mock, patch
 
 import yadtshell
 
-from yadtshell.actionmanager import ActionManager
+from yadtshell.actionmanager import (ActionManager,
+                                     _user_should_acknowledge_plan)
+
+
+class ActionManagerHelperFunctionsTest(TestCase):
+
+    @patch('yadtshell.actionmanager.sys.stdout')
+    def test_should_not_prompt_when_terminal_is_not_a_tty(self,
+                                                          mock_sys):
+        mock_sys.isatty.return_value = False
+
+        self.assertFalse(_user_should_acknowledge_plan(dryrun=False, flavor='update'))
+
+    @patch('yadtshell.actionmanager.sys.stdout')
+    def test_should_prompt_when_terminal_is_a_tty(self,
+                                                  mock_sys):
+        mock_sys.isatty.return_value = True
+
+        self.assertTrue(_user_should_acknowledge_plan(dryrun=False, flavor='update'))
+
+    @patch('yadtshell.actionmanager.sys.stdout')
+    def test_should_not_prompt_when_terminal_is_a_tty_but_dryrun_is_true(self,
+                                                                         mock_sys):
+        mock_sys.isatty.return_value = True
+
+        self.assertFalse(_user_should_acknowledge_plan(dryrun=True, flavor='update'))
 
 
 class ActionManagerActionTests(TestCase):
@@ -72,30 +97,3 @@ class ActionManagerActionTests(TestCase):
         self.am.action('update')
 
         self.assertFalse(mock_stop_and_return.called)
-
-    @patch('yadtshell.actionmanager.print', create=True)
-    @patch('yadtshell.actionmanager.confirm_transaction_by_user')
-    @patch('yadtshell.actionmanager.sys.stdout')
-    @patch('yadtshell.twisted.stop_and_return')
-    @patch('yadtshell.actionmanager.yaml.load')
-    @patch('yadtshell.actionmanager.open', create=True)
-    @patch('yadtshell.util.restore_current_state')
-    def test_should_not_prompt_when_no_tty(self,
-                                           components,
-                                           mock_open,
-                                           mock_load_action_plan,
-                                           mock_stop_and_return,
-                                           mock_stdout,
-                                           mock_transaction,
-                                           _):
-        mock_stdout.isatty.return_value = False
-        noop = Mock()
-        noop.cmd = 'harmless'
-        dangerous = Mock()
-        dangerous.cmd = 'reboot'
-        mock_load_action_plan.return_value.list_actions = [noop, dangerous]
-        self.user_accepts_transaction()
-
-        self.am.action('update')
-
-        self.assertFalse(mock_transaction.called)
