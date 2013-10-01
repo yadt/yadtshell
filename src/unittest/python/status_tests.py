6fa0b95fc4347e916e2066bdc926d066510ced36
mriehl <maximilien.riehl@gmail.com>
2013-10-01 09:12:05 +0200

    first unit test for status

---
+++ src/unittest/python/status_tests.py
@@ -0,0 +1,19 @@
+import yadtshell
+import unittest
+from mock import Mock, patch
+
+
+class StatusTests(unittest.TestCase):
+
+    def setUp(self):
+        yadtshell.settings.ybc = Mock()
+        yadtshell.settings.TARGET_SETTINGS = {
+            'name': 'test', 'hosts': ['foobar42']}
+
+    @patch('yadtshell._status.glob')
+    @patch('yadtshell._status.os')
+    def test_should_remove_old_state_files_when_calling_status(self, os, glob):
+        yadtshell.status()
+
+        os.path.join.assert_called_with(yadtshell.settings.OUT_DIR, 'current_state*')
+        os.remove.assert_called_with(os.path.join.return_value)
