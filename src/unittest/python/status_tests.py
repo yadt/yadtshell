72400851845bf98794dd7d5455fee2c642cf77bc
mriehl <maximilien.riehl@gmail.com>
2013-10-01 17:34:20 +0200

    adding unit test for removing explicit host dump files

--- src/unittest/python/status_tests.py
+++ src/unittest/python/status_tests.py
@@ -13,8 +13,16 @@ class StatusTests(unittest.TestCase):
 
     @patch('yadtshell._status.glob')
     @patch('yadtshell._status.os')
-    def test_should_remove_old_state_files_when_calling_status(self, os, glob):
+    def test_should_remove_globbed_old_state_files_when_calling_status(self, os, glob):
         yadtshell.status()
 
         os.path.join.assert_called_with(yadtshell.settings.OUT_DIR, 'current_state*')
+        os.remove.assert_called_with(os.path.join.return_value)
+
+    @patch('yadtshell._status.glob')
+    @patch('yadtshell._status.os')
+    def test_should_remove_old_state_files_explicity_when_calling_status_with_hosts(self, os, glob):
+        yadtshell.status(hosts=['foobar42'])
+
+        os.path.join.assert_called_with(yadtshell.settings.OUT_DIR, 'current_state_foobar42.yaml')
         os.remove.assert_called_with(os.path.join.return_value)
