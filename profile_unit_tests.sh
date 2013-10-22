db622bfb38021a542131d4e3506dee777760c901
mriehl <maximilien.riehl@gmail.com>
2013-10-22 09:54:06 +0200

    removing destructive line break

--- profile_unit_tests.sh
+++ profile_unit_tests.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-pip install gprof2dot && python -m cProfile -o output.pstats venv/bin/pyb run_unit_tests && gprof2dot -f pstats output.pstat
-s | dot -Tpng -o profiling_results.png
+pip install gprof2dot && python -m cProfile -o output.pstats venv/bin/pyb run_unit_tests && gprof2dot -f pstats output.pstats | dot -Tpng -o profiling_results.png
