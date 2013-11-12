4215d8161126fc310b14b285d74b50c3238163ce
Daniel Clerc <mail@clerc.eu>
2013-11-12 09:53:45 +0100

    PEP8 compliance for helper_tests

--- src/unittest/python/helper_tests.py
+++ src/unittest/python/helper_tests.py
@@ -1,18 +1,16 @@
 import unittest
 
-import yadtshell
-
 from yadtshell.helper import condense_hosts
 from yadtshell.helper import condense_hosts2
 
 
 class HelperTests(unittest.TestCase):
 
-
     def test_condense_hosts(self):
         TARGET_SETTINGS = {
-          'hosts': ["foobar01","foobar02", "foobar03", "bazbar01", "bazbar02", "bazbar03"]}
-	result = ' '.join(condense_hosts2(condense_hosts(TARGET_SETTINGS['hosts'])))
+            'hosts': ["foobar01", "foobar02", "foobar03",
+                       "bazbar01", "bazbar02", "bazbar03"]}
+        result = ' '.join(
+            condense_hosts2(condense_hosts(TARGET_SETTINGS['hosts'])))
         condensed_hosts = '{baz,foo}bar[01..03]'
-	self.assertEqual(result, condensed_hosts)
-
+        self.assertEqual(result, condensed_hosts)
