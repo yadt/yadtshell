6e802dc70cc02bc713cc86740d27fbe0ffc01239
mriehl <max@riehl.io>
2014-04-01 14:39:03 +0200

    lint with frosted additionally

--- build.py
+++ build.py
@@ -23,6 +23,7 @@ use_plugin('python.install_dependencies')
 use_plugin('python.unittest')
 use_plugin('python.coverage')
 use_plugin('python.flake8')
+use_plugin('python.frosted')
 
 use_plugin('python.distutils')
 use_plugin('copy_resources')
