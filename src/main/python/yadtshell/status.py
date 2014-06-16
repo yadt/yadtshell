13960add7d07aa020dbaf581fc76c78a6c2ce023
mriehl <max@riehl.io>
2014-06-16 15:02:00 +0200

    avoid chaining overloaded + and string format, thx @esc

--- src/main/python/yadtshell/status.py
+++ src/main/python/yadtshell/status.py
@@ -78,7 +78,7 @@ def handle_unreachable_host(failure, components):
 
 def write_host_data_to_file(host, host_data):
     host.replace("host://", "")
-    file_path = yadtshell.settings.log_file + ".%s.status" % host
+    file_path = "%s.%s.status" % (yadtshell.settings.log_file, host)
     logger.debug("Status of %s is at %s" % (host, file_path))
     with open(file_path, "w") as status_file:
         status_file.write(host_data)
