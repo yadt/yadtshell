28f279a14fe53d9177ed10a11be1a091366f4255
mriehl <max@riehl.io>
2014-12-19 10:24:49 +0100

    use log_exceptions to guard reboot()

--- src/main/python/yadtshell/reboot.py
+++ src/main/python/yadtshell/reboot.py
@@ -28,68 +28,62 @@ from yadtshell.metalogic import (metalogic,
                                  chop_minimal_related_chunks)
 from yadtshell.constants import REBOOT_REQUIRED
 from yadtshell.settings import DOWN
-from yadtshell.util import restore_current_state, dump_action_plan
+from yadtshell.util import restore_current_state, dump_action_plan, log_exceptions
 
 logger = logging.getLogger('reboot')
 
-
+@log_exceptions(logger)
 def reboot(protocol=None, uris=None, parallel=None, **kwargs):
-    try:
-        for uri in uris:
-            if not uri.startswith("host://"):
-                message = "Cannot reboot %s" % uri
-                logger.error(message)
-                raise ValueError(message)
+    for uri in uris:
+        if not uri.startswith("host://"):
+            message = "Cannot reboot %s" % uri
+            logger.error(message)
+            raise ValueError(message)
 
-        components = restore_current_state()
-        host_uris = expand_hosts(uris)
-        host_uris = glob_hosts(components, host_uris)
+    components = restore_current_state()
+    host_uris = expand_hosts(uris)
+    host_uris = glob_hosts(components, host_uris)
 
-        hosts_to_reboot = uris
-        stop_plan = create_plan_to_stop_all_services_on(hosts_to_reboot)
+    hosts_to_reboot = uris
+    stop_plan = create_plan_to_stop_all_services_on(hosts_to_reboot)
 
-        all_stopped_services = set()
-        for action in stop_plan.actions:
-            all_stopped_services.add(action.uri)
+    all_stopped_services = set()
+    for action in stop_plan.actions:
+        all_stopped_services.add(action.uri)
 
-        start_plan = create_plan_to_start_services_after_rebooting(all_stopped_services,
-                                                                   hosts_to_reboot,
-                                                                   components)
+    start_plan = create_plan_to_start_services_after_rebooting(all_stopped_services,
+                                                               hosts_to_reboot,
+                                                               components)
 
-        reboot_actions = set([create_reboot_action_for(components[host_uri]) for host_uri in hosts_to_reboot])
+    reboot_actions = set([create_reboot_action_for(components[host_uri]) for host_uri in hosts_to_reboot])
 
-        all_actions = set(start_plan.actions) | set(
-            stop_plan.actions) | reboot_actions
-        all_plan = ActionPlan('all', all_actions)
-        all_plan = chop_minimal_related_chunks(all_plan)
+    all_actions = set(start_plan.actions) | set(
+        stop_plan.actions) | reboot_actions
+    all_plan = ActionPlan('all', all_actions)
+    all_plan = chop_minimal_related_chunks(all_plan)
 
-        reboot_chunks = set()
-        for chunk in all_plan.actions:
-            all_chunk_cmds = set(a.cmd for a in chunk.actions)
-            if yadtshell.settings.UPDATE in all_chunk_cmds:
-                reboot_chunks.add(chunk)
-                continue
+    reboot_chunks = set()
+    for chunk in all_plan.actions:
+        all_chunk_cmds = set(a.cmd for a in chunk.actions)
+        if yadtshell.settings.UPDATE in all_chunk_cmds:
+            reboot_chunks.add(chunk)
+            continue
 
-        prestart_chunks = set()
-        for possible_prestart_chunk in all_plan.actions:
-            if possible_prestart_chunk in reboot_chunks:
-                continue
-            if possible_prestart_chunk.is_not_empty:
-                prestart_chunks.add(possible_prestart_chunk)
+    prestart_chunks = set()
+    for possible_prestart_chunk in all_plan.actions:
+        if possible_prestart_chunk in reboot_chunks:
+            continue
+        if possible_prestart_chunk.is_not_empty:
+            prestart_chunks.add(possible_prestart_chunk)
 
-        plan = ActionPlan(
-            'update', [ActionPlan('prestart', prestart_chunks),
-                       ActionPlan('stoprebootstart', reboot_chunks)
-                       ], nr_workers=1)
-        plan = apply_instructions(plan, parallel)
-        dump_action_plan('reboot', plan)
+    plan = ActionPlan(
+        'update', [ActionPlan('prestart', prestart_chunks),
+                   ActionPlan('stoprebootstart', reboot_chunks)
+                   ], nr_workers=1)
+    plan = apply_instructions(plan, parallel)
+    dump_action_plan('reboot', plan)
 
-        return 'reboot'
-    except BaseException as e:
-        logger.error("Problem white creating plan for reboot: %s" % e)
-        import traceback
-        logger.debug(traceback.format_exc(e))
-        raise
+    return 'reboot'
 
 
 def create_reboot_action_for(host):
