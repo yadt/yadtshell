2384218c62d744986235b9099cb208a473c6bb18
mriehl <max@riehl.io>
2015-02-12 15:19:43 +0100

    remove redundant property, now autodetected

--- build.py
+++ build.py
@@ -112,7 +112,6 @@ def set_properties(project):
 @init(environments='teamcity')
 def set_properties_for_teamcity_builds(project):
     import os
-    project.set_property('teamcity_output', True)
     project.version = '%s-%s' % (project.version, os.environ.get('BUILD_NUMBER', 0))
     project.default_task = ['clean', 'install_build_dependencies', 'publish']
     project.set_property('install_dependencies_index_url', os.environ.get('PYPIPROXY_URL'))
